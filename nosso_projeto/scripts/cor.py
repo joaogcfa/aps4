ae3f05bddc30dd25cafac6314e73c1e68c5dfeb1
borg <joaogcfa@al.insper.edu.br>
2020-03-31 18:37:33 -0300

    adicionamos a funcao de chegar a 50 cm e muda para laser e chegar a 30 cm e parar, so que ainda vai de um lado para o outro

--- nosso_projeto/scripts/cor.py
+++ nosso_projeto/scripts/cor.py
@@ -1,6 +1,5 @@
 #! /usr/bin/env python
 # -*- coding:utf-8 -*-
-
 __author__ = ["Rachel P. B. Moraes", "Igor Montagner", "Fabio Miranda"]
 
 
@@ -12,10 +11,9 @@ import cv2
 import time
 from geometry_msgs.msg import Twist, Vector3, Pose
 from nav_msgs.msg import Odometry
-from sensor_msgs.msg import Image, CompressedImage
+from sensor_msgs.msg import Image, CompressedImage, LaserScan
 from cv_bridge import CvBridge, CvBridgeError
 import cormodule
-
 
 bridge = CvBridge()
 
@@ -30,31 +28,45 @@ area = 0.0 # Variavel com a area do maior contorno
 # Descarta imagens que chegam atrasadas demais
 check_delay = False 
 
+maior_area = 0
+
 # A função a seguir é chamada sempre que chega um novo frame
 def roda_todo_frame(imagem):
-	print("frame")
+	# print("frame")
 	global cv_image
 	global media
 	global centro
-
+	global maior_area
+	
 	now = rospy.get_rostime()
 	imgtime = imagem.header.stamp
 	lag = now-imgtime # calcula o lag
 	delay = lag.nsecs
-	print("delay ", "{:.3f}".format(delay/1.0E9))
+	# print("delay ", "{:.3f}".format(delay/1.0E9))
 	if delay > atraso and check_delay==True:
-		print("Descartando por causa do delay do frame:", delay)
+		# print("Descartando por causa do delay do frame:", delay)
 		return 
 	try:
 		antes = time.clock()
 		cv_image = bridge.compressed_imgmsg_to_cv2(imagem, "bgr8")
 		# cv_image = cv2.flip(cv_image, -1)
+		# área: maior_area
 		media, centro, maior_area =  cormodule.identifica_cor(cv_image)
 		depois = time.clock()
 		cv2.imshow("Camera", cv_image)
 	except CvBridgeError as e:
 		print('ex', e)
-	
+
+
+# Inserir o código que vai receber o evento do laser. E também guardar a distância 
+# do dado.ranges[0] - tem no le_scan.py
+
+distancia = 0
+def scaneou(dado):
+	global distancia
+	#distancia = np.array(dado.ranges).round(decimals=2)[0]
+	distancia = dado.ranges[0]
+	# print("A DISTANCIA eh no def", distancia)
 if __name__=="__main__":
 	rospy.init_node("cor")
 
@@ -81,35 +93,78 @@ if __name__=="__main__":
 	# 
 
 	recebedor = rospy.Subscriber(topico_imagem, CompressedImage, roda_todo_frame, queue_size=4, buff_size = 2**24)
-	print("Usando ", topico_imagem)
+	## trazer / criar um Subscriber do tópico /scan - le_scan.py
+	
+	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 3 )
+	recebe_scan = rospy.Subscriber("/scan", LaserScan, scaneou)
 
-	velocidade_saida = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
+	#print("Usando ", topico_imagem)
+	
+	ta_no_centro = False
+	chegou_distancia = False
+
+	menor_media = 310
+	maior_media = 330
 
 	try:
+		while not chegou_distancia:
+			while not rospy.is_shutdown():
+				vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
+				if ta_no_centro == False: 
+					if len(media) != 0 and len(centro) != 0:
+						
+						# print("Média dos verdes: {0}, {1}".format(media[0], media[1]))
+						# print("Centro dos verdes: {0}, {1}".format(centro[0], centro[1]))
+						# print("A DISTANCIA É", distancia)
 
-		while not rospy.is_shutdown():
 
-			vel = Twist(Vector3(0,0,0), Vector3(0,0,0))
-			ta_no_centro = False
-			while not ta_no_centro: 
-				if len(media) != 0 and len(centro) != 0:
-					
-					print("Média dos verdes: {0}, {1}".format(media[0], media[1]))
-					print("Centro dos verdes: {0}, {1}".format(centro[0], centro[1]))
+						# Identificar onde está o verde e manter centralizado
+						if (media[0] > centro[0]):
+							vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
+						if (media[0] < centro[0]):
+							vel = Twist(Vector3(0,0,0), Vector3(0,0,0.1))
+						if ((menor_media<media[0]) and (media[0]<maior_media)):
+							ta_no_centro = True
 
-					# Identificar onde está o verde e manter centralizado
-					if (media[0] > centro[0]):
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,-0.1))
-					if (media[0] < centro[0]):
-						vel = Twist(Vector3(0,0,0), Vector3(0,0,0.1))
-					if (media[1]>210 & 320>media[0]):
-						ta_no_centro = True
+					velocidade_saida.publish(vel)
+					rospy.sleep(0.1)
 
-			if len(media) != 0 and len(centro) != 0:
-				vel = Twist(Vector3(0.8,0,0), Vector3(0,0,0))
-			
-			velocidade_saida.publish(vel)
-			rospy.sleep(0.1)
+				else:		
+
+					if len(media) != 0 and len(centro) != 0:
+
+						vel = Twist(Vector3(0.3,0,0), Vector3(0,0,0))
+
+						# print("Média: {0}, {1}".format(media[0], media[1]))
+						# print("Centro: {0}, {1}".format(centro[0], centro[1]))
+						# print("A DISTANCIA EH", distancia)
+
+						
+						if distancia < 0.50:
+
+							velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+							velocidade_saida.publish(velocidade)
+							rospy.sleep(0.6)
+							print("A DISTANCIA eh", distancia)
+							while distancia > 0.30:
+								velocidade = Twist(Vector3(0.3, 0, 0), Vector3(0, 0, 0))
+								velocidade_saida.publish(velocidade)
+								rospy.sleep(0.2)
+
+								print("A DISTANCIA EH", distancia)
+
+							while distancia < 0.30:
+								print("HI")
+								velocidade = Twist(Vector3(0, 0, 0), Vector3(0, 0, 0))
+								velocidade_saida.publish(velocidade)
+								rospy.sleep(0.2)
+								break								
+
+						if not ((menor_media<media[0]) and (media[0]<maior_media)):
+							ta_no_centro = False
+
+					velocidade_saida.publish(vel)
+					rospy.sleep(0.25)
 
 	except rospy.ROSInterruptException:
-	    print("Ocorreu uma exceção com o rospy")
+		print("Ocorreu uma exceção com o rospy")
